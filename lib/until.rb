461875e00ccfe0b0636918c71c0bd62b515b1afd
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-13 15:05:52 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,7 @@
-def using_until = "Magic"
   levitation_force = 6
-until levitation_force == 10 
-puts "Wingardium Leviosa"
-levitation_force += 1 
+while levitation_force < 10 
+  puts "Wingardium Leviosa"
+  counter +=1
 end
 
 
